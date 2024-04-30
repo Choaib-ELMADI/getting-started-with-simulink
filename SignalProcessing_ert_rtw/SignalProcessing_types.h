03ef30b818998c7c18ce4808e16d5a82c38fcb7e
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-30 01:40:58 +0100

    Simulink for signals processing

---
+++ SignalProcessing_ert_rtw/SignalProcessing_types.h
@@ -0,0 +1,32 @@
+/*
+ * File: SignalProcessing_types.h
+ *
+ * Code generated for Simulink model 'SignalProcessing'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 8.14 (R2018a) 06-Feb-2018
+ * C/C++ source code generated on : Tue Apr 30 01:39:11 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_SignalProcessing_types_h_
+#define RTW_HEADER_SignalProcessing_types_h_
+#include "rtwtypes.h"
+
+/* Parameters (default storage) */
+typedef struct P_SignalProcessing_T_ P_SignalProcessing_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_SignalProcessing_T RT_MODEL_SignalProcessing_T;
+
+#endif                                 /* RTW_HEADER_SignalProcessing_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ SignalProcessing_ert_rtw/rtmodel.h
@@ -0,0 +1,30 @@
+/*
+ * File: rtmodel.h
+ *
+ * Code generated for Simulink model 'SignalProcessing'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 8.14 (R2018a) 06-Feb-2018
+ * C/C++ source code generated on : Tue Apr 30 01:39:11 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+#include "SignalProcessing.h"
+
+/* Macros generated for backwards compatibility  */
+#ifndef rtmGetStopRequested
+# define rtmGetStopRequested(rtm)      ((void*) 0)
+#endif
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
